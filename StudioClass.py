f0ab945cac6abf99a4c60773938875b0b666263a
Yuri <47018053+GorgeousG30rge@users.noreply.github.com>
2020-03-19 20:09:14 +0500

    Update StudioClass.py

--- StudioClass.py
+++ StudioClass.py
@@ -1,41 +1,5 @@
 import random
 
-
-class Stats:
-    def __init__(self, company):
-        self.reporting_company = company
-
-    def show_design_results(self):
-        print('Design Dept')
-        self.reporting_company.__design.get_test_results()
-        print()
-    
-    def show_front_results(self):
-        print('Front Dept')
-        self.reporting_company.__front.get_test_results()
-        print()
-
-    def show_back_results(self):
-        print('Back Dept')
-        self.reporting_company.__back.get_test_results()
-        print()
-
-    def show_design_totals(self):
-        print('Design Dept')
-        self.reporting_company.__design.get_totals()
-        print()
-    
-    def show_front_totals(self):
-        print('Front Dept')
-        self.reporting_company.__front.get_totals()
-        print()
-
-    def show_back_totals(self):
-        print('Back Dept')
-        self.reporting_company.__back.get_totals()
-        print()
-
-
 class Repository:
     def create_person(self):
         Male_names = ['Alexander', 'Alexey', 'Andrey', 'Boris', 'Petr', 'Vladimir', 'Kirill']
